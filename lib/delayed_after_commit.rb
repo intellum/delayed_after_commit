d875a789d07cc88d4a4404bc3a0381343d51c728
Simon Rentzke <simon@rentzke.com>
2016-10-21 16:30:01 +0200

    Better options support.

--- lib/delayed_after_commit.rb
+++ lib/delayed_after_commit.rb
@@ -11,23 +11,27 @@ module DelayedAfterCommit
       obj.send(method)
     end
 
-    def delayed_after_create(method, &block)
-      delayed_after_commit(method, on: :create, &block)
+    def delayed_after_create(*args, &block)
+      args << {on: :create}
+      delayed_after_commit(*args, &block)
     end
 
-    def delayed_after_update(method, &block)
-      delayed_after_commit(method, on: :update, &block)
+    def delayed_after_update(*args, &block)
+      args << {on: :update}
+      delayed_after_commit(*args, &block)
     end
 
     protected
-    def delayed_after_commit(method, on:, &block)
+    def delayed_after_commit(*args, &block)
       # this creates a method that runs `enqueue_delayed_method`
       # it then adds that method to the after_commit callback
-      delayed_method_name = "delayed_after_#{on}_#{method}"
+      opts = args.extract_options!
+      method = args.first
+      delayed_method_name = "delayed_after_#{opts[:on]}_#{method}"
       define_method(delayed_method_name) do |m = method|
         self.class.delay.enqueue_delayed_method(m, self.id)
       end
-      self.after_commit(delayed_method_name.to_sym, on: on, &block)
+      self.after_commit(delayed_method_name.to_sym, opts, &block)
     end
   end
 end
--- lib/delayed_after_commit/version.rb
+++ lib/delayed_after_commit/version.rb
@@ -1,3 +1,3 @@
 module DelayedAfterCommit
-  VERSION = "0.1.3"
+  VERSION = "0.1.4"
 end
