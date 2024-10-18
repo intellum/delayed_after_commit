83e34f5be75f4b4dfbbcc6e1e3f57f52cbd55ab1
Adit Saxena <a.saxena.email@gmail.com>
2024-10-18 13:54:37 +0200

    Update deprecated .delay Sidekiq method
    The .delay method has been deprecated since it could allow
    users to inadvertently create background jobs with large payloads.
    
    With this commit introduces a worker to schedule jobs upon commit.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 
 # Specify your gem's dependencies in delayed_after_commit.gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "rubygems"
 require "bundler"
 require "rspec"
@@ -6,13 +8,17 @@ require 'sidekiq/testing'
 require "delayed_after_commit"
 require 'byebug'
 
-Sidekiq::Extensions.enable_delay!
+Sidekiq::Testing.fake!
 
 ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
 ActiveRecord::Migration.verbose = false
 
-ActiveRecord::Schema.define :version => 0 do
-  create_table(:users)  { |t| t.string :name; t.integer :number_of_updates; t.integer :number_of_letters_in_name }
+ActiveRecord::Schema.define version: 0 do
+  create_table(:users) do |t|
+    t.string :name
+    t.integer :number_of_updates
+    t.integer :number_of_letters_in_name
+  end
 end
 
 class User < ActiveRecord::Base
@@ -25,15 +31,17 @@ class User < ActiveRecord::Base
   delayed_after_update :failing_callback, if: :fail_enabled, retry_max: 3
 
   protected
+
   def calculate_number_of_letters_in_name
-    update_column('number_of_letters_in_name', self.name.size)
+    update_column('number_of_letters_in_name', name.size)
   end
 
   def increment_number_of_updates
-    update_column('number_of_updates', self.number_of_updates.to_i + 1)
+    update_column('number_of_updates', number_of_updates.to_i + 1)
   end
 
   def failing_callback
+    increment_number_of_updates
     raise "This fails"
   end
 end
