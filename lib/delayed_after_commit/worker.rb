0a595659aaeb2c9be161e5dce2c90a64fd503d6c
Adit Saxena <a.saxena.email@gmail.com>
2024-10-18 13:54:37 +0200

    Update deprecated .delay Sidekiq method
    The .delay method has been deprecated since it could allow
    users to inadvertently create background jobs with large payloads.
    
    With this commit introduces a worker to schedule jobs upon commit.

---
+++ lib/delayed_after_commit/worker.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module DelayedAfterCommit
+  # This is the worker that runs the delayed methods after a record is created or updated
+  class Worker
+    include Sidekiq::Job
+
+    sidekiq_options retry: false
+
+    def perform(class_name, method, id, retry_max = nil, retry_count = 0, queue = 'default')
+      klass = class_name.constantize
+      obj = klass.find_by(id:)
+      begin
+        obj.send(method) if obj.present?
+      rescue StandardError => e
+        if retry_max.present? && retry_count < retry_max
+          self.class.perform_in(retry_count.minutes, class_name, method, id, retry_max, retry_count + 1, queue.to_s)
+        end
+        raise e
+      end
+    end
+  end
+end
